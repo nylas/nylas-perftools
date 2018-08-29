975d1aef8580703191ce7ed03bfedfadf0a9a875
amorgun <amorgun@users.noreply.github.com>
2018-08-29 19:30:51 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ setup(
         'click',
         'dateparser'
     ],
-    include_package_data=True
+    package_dir={'stackcollector': 'nylas-perftools/stackcollector'},
+    package_data={'stackcollector': ['static/*']},
 )
