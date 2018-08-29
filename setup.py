cc199c561d2d4c199e763f87c0702ad3f6c99054
amorgun <amorgun@users.noreply.github.com>
2018-08-29 19:32:35 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='stackcollector',
     version='0.1',
-    packages=find_packages(),
+    packages=['stackcollector'],
     install_requires=[
         'requests>=2.4.3',
         'flask>=0.10.1',
